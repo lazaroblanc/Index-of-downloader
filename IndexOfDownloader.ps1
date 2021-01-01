76ee3150ffd517927d5473a3c996e03bd3fde57d
Lázaro Blanc <lazaroblanc@users.noreply.github.com>
2021-01-01 19:48:08 +0100

    Added HTTP authentication support. Now also downloads the correct wget binary depending on 64/32-bit host

--- IndexOfDownloader.ps1
+++ IndexOfDownloader.ps1
@@ -3,21 +3,41 @@ Add-Type -AssemblyName Microsoft.VisualBasic
 
 # Prompt for Url
 $title = '"Index of" downloader'
-$msg = @"
-EN: Please enter URL
-
-DE: Bitte URL eingeben
-"@
-
+$msg = "Please enter URL"
 $Url = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title, "https://...")
 
-if ($Url.length -eq 0) {
-    exit
-}
-
+if ($Url.length -eq 0) { exit }
 if (-not [Uri]::IsWellFormedUriString($Url, 1)) {
     "Malformed Url" | Write-Error
     exit
+}
+
+$NoOfDirsToCut = ([Uri]$Url).Segments.Count - 1
+$wgetParams = @(
+    "--no-check-certificate"
+    "--recursive"
+    "--no-clobber"
+    "--no-parent"
+    "--no-host-directories"
+    "--cut-dirs=$NoOfDirsToCut"
+    "--quiet"
+    "--show-progress"
+)
+
+try { $statusCode = (Invoke-WebRequest -Uri $Url -ErrorAction Stop).StatusCode }
+catch { $statusCode = [int]$_.Exception.Response.StatusCode; $_ | Write-Error }
+while ($statusCode -eq 401) {
+    $creds = Get-Credential -Message "401 - The website requires a username/password"
+    if ($null -eq $creds) { exit }
+    try {
+        $statusCode = (Invoke-WebRequest -Uri $Url -Credential $creds -ErrorAction Stop).StatusCode
+        $creds = New-Object psobject -Property @{"user" = $creds.UserName; "password" = $creds.GetNetworkCredential().password }
+        $wgetParams += "--http-user=$($creds.user)"
+        $wgetParams += "--http-password=$($creds.password)"
+    }
+    catch {
+        $_ | Write-Error
+    }
 }
 
 # Prompt for download location
@@ -28,27 +48,22 @@ Select download directory
 Downloadverzeichnis auswählen
 "@
 $buttonPressed = $folderBrowseDialog.ShowDialog()
-
-if ($buttonPressed -eq "Cancel") {
-    exit
-}
-
+if ($buttonPressed -eq "Cancel") { exit }
 Set-Location $folderBrowseDialog.SelectedPath
 
-$wget = "wget.exe"
 # Only download wget if not already found on system
-if (-not (Get-Command wget.exe -ErrorAction SilentlyContinue)) {
-    Invoke-WebRequest -Uri "https://eternallybored.org/misc/wget/1.20.3/64/wget.exe" -OutFile ".\$wget"
+$wget = "wget.exe"
+if (-not (Get-Command $wget -ErrorAction SilentlyContinue)) {
+    if ([Environment]::Is64BitOperatingSystem) { $architecture = "64" }
+    else { $architecture = "32" }
+    Invoke-WebRequest -Uri "https://eternallybored.org/misc/wget/1.20.3/$architecture/wget.exe" -OutFile ".\$wget"
     $cleanupWget = $true
-    $wget = ".\wget.exe"
+    $wget = ".\$wget"
 }
 
-$NoOfDirsToCut = ([Uri]$Url).Segments.Count - 1
-& $wget --no-check-certificate --recursive --no-clobber --no-parent --no-host-directories --cut-dirs=$NoOfDirsToCut --quiet --show-progress --execute robots=off --reject index.htm* $Url
+& $wget $wgetParams --execute robots=off --reject index.htm* $Url
 
-if ($cleanupWget) {
-    $wget | Remove-Item
-}
+if ($cleanupWget) { $wget | Remove-Item }
 
 Read-Host "Press any key to exit"
 exit
--- README.md
+++ README.md
@@ -14,21 +14,29 @@
 1. <a href="https://github.com/lazaroblanc/indexof-downloader/releases/latest">Download the latest release</a>
 1. Run the script `DownloadIndexOf.ps1` via:
     1. `Right-Click` and selecting `Run with PowerShell`
-    1. or by calling it from the command-line: `PS > DownloadIndexOf.ps1`
+    1. the command-line: `.\DownloadIndexOf.ps1`
+
 
 <div align="center">
 <img src="https://i.imgur.com/ectqjd1.gif">
 </div>
 
-## Why not just use `wget`?
+## Features
 
-I occasionally find myself sharing large files with friends and family that I have hosted on a private webserver.
+- uses `wget` to retrieve files
+- uses GUI elements for prompts to the user
+- Progress indicators
+- Support for HTTP authentication methods
+
+## Why not just use `wget` from the cli?
+
+I've occasionally found myself sharing large files with friends and family that I have hosted on a private webserver.
 
 Since I usually split very large files into multiple parts I wanted them to be able to download the multiple files easily without the need to get additional software or knowing how to use it (like `wget` or *JDownloader*)
 
 This does exactly that while being *user-friendly* at the same time.<br>
 The user gets a WinForms GUI prompt for entering the URL and the download location.
-Afterwards the download is started and shown with a progress indicator (courtesy of `wget`)
+Afterwards the download is started and shown with a progress indicator.
 
 Now I can just give them this script with a URL to where the downloads are *et-voila*: No hassle for me or for them! :)
 
@@ -36,11 +44,7 @@ Now I can just give them this script with a URL to where the downloads are *et-v
 
 1. While this Script will run in Windows PowerShell (v3.0 - v5.1) and PowerShell (>= v6) it **works only under Windows** since it uses the Windows Binaries for `wget` and the WinForms library for the GUI elements
 
-1. If `wget` is not installed on the target machine it will download the 64-bit Binary. Thus this script **will not work on 32-bit hosts**.
-
 1. I've used *reasonable* arguments for `wget` in the script. There currently is no need to use different arguments nor is there a way to supply your own arguments to the `wget` instance in this script. Feel free to modify them in the script itself or add a way for you to pass them into the script via a parameter(s). In that case I would welcome a pull request! :) I however do not currently need this functionality.
-
-1. No support for HTTP authentication methods. You can pass `--http-user` and `--http-password` as an argument to `wget` yourself. (See above)
 
 <div align="center">
 <hr>
